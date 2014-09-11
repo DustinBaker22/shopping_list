011bca7814ac99d2d434fcdf9d916bed54e235bf
Dustin Baker <Dustin.T.Baker22@gmail.com>
2014-09-11 10:00:14 -0500

    Added Functions
    Added functions such as clicking on items to strike them out and double
    clicking on items to delete them.

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-	alert('Oh, Hi there, World.');
+	$('#button').on('click',function() {
+		var items= $('#item').val();
+		$('.shopping-list').append('<li>'+items+'</li>');
+	});
+	$('#item').keyup(function(e) {
+		if (e.keyCode==13){
+			var items=$(this).val();
+			$('.shopping-list').append('<li>'+items+'</li>');			
+			}
+	});
+	$('.shopping-list').on('click','li',function() {
+		$(this).toggleClass('strike');
+	});
+	$('.shopping-list').on('dblclick','li',function() {
+		$(this).addClass('hide');
+	});
 });
+
+/* Add two functions on is on click to strick through (addclass) and .on double click to .remove items*/
